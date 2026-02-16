7848109cb38a1f908b5b42eccf7238ec8b972518
Muhammad Hatta <muhammadhatta834@gmail.com>
2026-02-16 17:26:52 +0700

    fast api only

--- api/index.py
+++ api/index.py
@@ -1,11 +1,9 @@
 import sys
 import os
 
-# Add the root directory to sys.path so we can import from 'server' and 'app'
-# Vercel runs from the root, but we put this in api/
+# Add the root directory to sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from server import app
 
-# Vercel looks for the variable 'app' in api/index.py by default
-# Or we can specify it in vercel.json
+# Vercel looks for 'app'
